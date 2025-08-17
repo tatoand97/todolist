72b9a63c91ce307b16b08b501fb89c06e515d1ae
Ricardo Leyva <ricardoandres9728@hotmail.com>
2025-08-17 01:26:17 -0500

    feat: update user registration to use form data and add profile image support refactor: change GIN_MODE to debug in docker-compose chore: clean up SQL migration formatting for better readability

--- internal/infrastructure/migrations/0001_create_tables.up.sql
+++ internal/infrastructure/migrations/0001_create_tables.up.sql
@@ -1,25 +1,22 @@
-CREATE TABLE users
-(
-    id                SERIAL PRIMARY KEY,
-    username          TEXT NOT NULL UNIQUE,
-    password_hash     TEXT NOT NULL,
-    profile_image_url TEXT,
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    profile_image_url TEXT
 );
 
-CREATE TABLE categories
-(
-    id          SERIAL PRIMARY KEY,
-    name        TEXT    NOT NULL,
-    description TEXT,
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT
 );
 
-CREATE TABLE tasks
-(
-    id          SERIAL PRIMARY KEY,
-    text        TEXT      NOT NULL,
-    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    due_date    TIMESTAMP,
-    state       TEXT      NOT NULL CHECK (state IN ('Sin Empezar', 'Empezada', 'Finalizada')),
-    category_id INTEGER   NOT NULL REFERENCES categories (id) ON DELETE CASCADE,
-    user_id     INTEGER   NOT NULL REFERENCES users (id) ON DELETE CASCADE
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    text TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    due_date TIMESTAMP,
+    state TEXT NOT NULL CHECK (state IN ('Sin Empezar','Empezada','Finalizada')),
+    category_id INTEGER NOT NULL REFERENCES categories (id) ON DELETE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
