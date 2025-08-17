33b5eb7d65a9a73580874f756a48fd3e6724b89b
Ricardo A Leyva <25676407+tatoand97@users.noreply.github.com>
2025-08-16 23:51:46 -0500

    Guardar imagen de perfil en registro

--- internal/application/useCase/auth.go
+++ internal/application/useCase/auth.go
@@ -3,7 +3,11 @@ package useCase
 import (
 	"context"
 	"errors"
+	"fmt"
+	"mime"
+	"net/http"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 	"todolist/internal/domain/entities"
@@ -17,26 +21,56 @@ var defaultAvatar = "/static/default.jpg"
 
 type AuthService struct {
 	repo          interfaces.UserRepository
+	fileWriter    interfaces.FileWriter
 	jwtSecret     string
 	invalidTokens sync.Map // map[string]struct{}
 }
 
-func NewAuthService(repo interfaces.UserRepository, secret string) *AuthService {
-	return &AuthService{repo: repo, jwtSecret: secret}
+func NewAuthService(repo interfaces.UserRepository, writer interfaces.FileWriter, secret string) *AuthService {
+	return &AuthService{repo: repo, fileWriter: writer, jwtSecret: secret}
 }
 
-func (s *AuthService) Register(ctx context.Context, username, password string, profileImageURL *string) (*entities.User, error) {
+func (s *AuthService) Register(ctx context.Context, username, password string, profileImage []byte) (*entities.User, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return nil, err
 	}
-	if profileImageURL == nil {
-		profileImageURL = &defaultAvatar
+
+	imagePath := defaultAvatar
+
+	if len(profileImage) > 0 {
+		const maxSize = 5 * 1024 * 1024
+		if len(profileImage) > maxSize {
+			return nil, errors.New("image too large")
+		}
+		mimeType := http.DetectContentType(profileImage)
+		if !strings.HasPrefix(mimeType, "image/") {
+			return nil, errors.New("invalid image type")
+		}
+		var ext string
+		if exts, _ := mime.ExtensionsByType(mimeType); len(exts) > 0 {
+			ext = exts[0]
+		} else {
+			switch mimeType {
+			case "image/jpeg":
+				ext = ".jpg"
+			case "image/png":
+				ext = ".png"
+			default:
+				return nil, errors.New("unsupported image type")
+			}
+		}
+		name := fmt.Sprintf("%d%s", time.Now().UnixNano(), ext)
+		if err := s.fileWriter.Write(ctx, name, profileImage); err != nil {
+			return nil, err
+		}
+		imagePath = "/static/" + name
 	}
+
 	user := &entities.User{
 		Username:        username,
 		PasswordHash:    string(hash),
-		ProfileImageURL: profileImageURL,
+		ProfileImageURL: &imagePath,
 	}
 	if err := s.repo.Create(ctx, user); err != nil {
 		return nil, err
